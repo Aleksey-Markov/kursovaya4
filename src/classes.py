7d156fe491550cb76e4fd908dfe004bc93436856
Aleksey-Markov <tte4enuxa@gmail.com>
2024-03-11 23:20:47 +0700

    update

--- src/classes.py
+++ src/classes.py
@@ -1,3 +1,5 @@
+import json
+import os
 import requests
 from abc import ABC, abstractmethod
 
@@ -16,6 +18,23 @@ class HeadHunter(API):
         response = requests.get(self.url + '?text=' + keyword)
         return response.json()
 
+    @staticmethod
+    def save_to_json(data, file_to_save):
+        with open(file_to_save, 'w', encoding='utf8') as q:
+            json.dump(data, q, ensure_ascii=False, indent=4)
+
+
+class HHVacancies:
+    def __init__(self, name, city, link, salary, requirement, responsibility):
+        self.name = name
+        self.city = city
+        self.link = link
+        self.salary = salary
+        self.requirement = requirement
+        self.responsibility = responsibility
+
 
 hh = HeadHunter('https://api.hh.ru/vacancies')
-print(hh.get_vacancies('Python стажер'))
+dat = hh.get_vacancies('Python')
+files = hh.save_to_json(dat, 'vacancies.json')
+print(dat['items'][0]['salary']['from'])
